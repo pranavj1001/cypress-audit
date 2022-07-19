d4ee0dc20ee9f7d9fe9bc02ed8dee322d01dc3ef
Pranav Jain <pranavj1001@gmail.com>
2022-07-19 10:00:54 +0530

    add debug logs in task.js

--- packages/pa11y/src/task.js
+++ packages/pa11y/src/task.js
@@ -2,11 +2,17 @@ const pa11yLib = require("pa11y");
 const puppeteer = require("puppeteer");
 
 const pa11y = (callback) => async ({ url, opts }) => {
+  cy.log("cy.pa11y()", "Inside pa11y function");
+  
   const browser = await puppeteer.connect({
     browserURL: `http://localhost:${global.cypress_audit_port}`,
   });
+  
+  cy.log("cy.pa11y()", `Connected to browser using puppeteer ${browser}`);
 
   const results = await pa11yLib(url, { browser, runners: ["axe"], ...opts });
+  
+    cy.log("cy.pa11y()", `Fetched results ${results}`);
 
   if (callback) {
     callback(results);
